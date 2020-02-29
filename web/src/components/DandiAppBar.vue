94f944dccd0a93be99cd1e872fedc28b8635158c
Satrajit Ghosh <satra@mit.edu>
2020-02-29 17:22:14 -0500

    enh: add about button

--- web/src/components/DandiAppBar.vue
+++ web/src/components/DandiAppBar.vue
@@ -3,6 +3,15 @@
     <v-toolbar-title>
       <img align="center" alt="DANDI logo" height="48px" src="@/assets/logo.svg" />
     </v-toolbar-title>
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+        <a href="//www.dandiarchive.org">
+        <v-btn class="ml-2" color="secondary" v-on="on">About</v-btn>
+        </a>
+      </template>
+      <span>You are currently viewing the data portal.
+      Click this button to learn more about the DANDI project</span>
+    </v-tooltip>
     <v-tooltip right>
       <template v-slot:activator="{ on }">
         <v-chip class="ml-2" color="secondary" v-on="on">
