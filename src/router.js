ea7f8ae526ea5d1fc76bdca4fc8d25415c28bc75
Satrajit Ghosh <satrajit.ghosh@gmail.com>
2021-04-02 10:32:12 -0400

    Update router.js

--- src/router.js
+++ src/router.js
@@ -12,6 +12,7 @@ import CreateDandisetView from '@/views/CreateDandisetView/CreateDandisetView.vu
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
