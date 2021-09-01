4ecbaf422f46c85c7e06d0a6f7a0b9db48c15dd9
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2021-09-01 19:06:49 -0400

    Add test for publishing a dandiset

--- test/src/tests/dandisetLandingPage.test.js
+++ test/src/tests/dandisetLandingPage.test.js
@@ -1,4 +1,11 @@
-import { vChip, vListItem } from 'jest-puppeteer-vuetify';
+import {
+  vBtn,
+  vChip,
+  vIcon,
+  vListItem,
+  vTextField,
+} from 'jest-puppeteer-vuetify';
+
 import {
   uniqueId,
   registerNewUser,
@@ -6,6 +13,7 @@ import {
   logout,
   waitForRequestsToFinish,
   clearCookiesAndCache,
+  uploadAssets,
 } from '../util';
 
 describe('dandiset landing page', () => {
@@ -38,5 +46,39 @@ describe('dandiset landing page', () => {
     // otherUser should be an owner now, too
     await expect(page).toContainXPath(vChip(otherUser));
     await expect(page).toContainXPath(vChip(owner));
+  });
+
+  it('publish a dandiset', async () => {
+    const { apiKey } = await registerNewUser();
+
+    const id = uniqueId();
+    const name = `name ${id}`;
+    const description = `description ${id}`;
+    const identifier = await registerDandiset(name, description);
+
+    // Fix "no assets" validation error
+    // await expect(page).toContainXPath(vBtn('This Dandiset has 2 metadata'));
+    await uploadAssets(identifier, 'valid_assets', apiKey);
+    await page.waitForTimeout(2000);
+    await page.reload();
+    await waitForRequestsToFinish();
+    // await expect(page).toContainXPath(vBtn('This Dandiset has 1 metadata'));
+
+    // Fix license validation error
+    await expect(page).toClickXPath(vBtn('Metadata'));
+    await expect(page).toClickXPath('//input[@id="license"]');
+    await page.waitForTimeout(500);
+    await expect(page).toClickXPath(vListItem('spdx:CC0-1.0'));
+    await expect(page).toClickXPath(vTextField('Acknowledgement')); // click something else to trigger validation
+    await expect(page).toClickXPath(vIcon('mdi-content-save-alert'));
+    await page.waitForTimeout(2000); // give enough time for saving to finish
+    await expect(page).toClickXPath(vIcon('mdi-arrow-left')); // exit the meditor
+    await waitForRequestsToFinish();
+
+    // await expect(page).not.toContainXPath(vBtn('This Dandiset has 1 metadata'));
+
+    await expect(page).toClickXPath(vBtn('Publish'));
+
+    // TODO: make assertions about the published dandiset
   });
 });
