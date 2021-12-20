f1f4719d1a7acad11402bb5229675efa4db2f8e4
Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>
2021-12-20 15:59:41 -0500

    Lower number of dandisets created in test

--- test/src/tests/dandisetIdentifiers.test.js
+++ test/src/tests/dandisetIdentifiers.test.js
@@ -13,7 +13,7 @@ describe('ensures dandiset identifiers are incremented correctly', () => {
   it('creates a bunch of new dandisets while verifying that their IDs are correct', async () => {
     jest.setTimeout(60000 * 30); // Temporarily increase timeout to 30 mins for this test
 
-    // Create 2 users and create 500 dandisets with each of them
+    // Create 2 users and create 100 dandisets with each of them
     for (let i = 0; i < 2; i += 1) {
       // register user and create a new dandiset
       await registerNewUser();
@@ -26,7 +26,7 @@ describe('ensures dandiset identifiers are incremented correctly', () => {
 
       for (
         let latestIdentifier = Number(initialIdentifier);
-        latestIdentifier < Number(initialIdentifier) + 500;
+        latestIdentifier < Number(initialIdentifier) + 100;
         latestIdentifier += 1
       ) {
         const id = uniqueId();
