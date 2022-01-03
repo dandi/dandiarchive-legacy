0763c76b1c3ac8156bf858e990829c9d90328355
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2022-01-03 13:06:50 -0500

    Fix typescript errors introduced by node types

--- src/rest.ts
+++ src/rest.ts
@@ -5,6 +5,10 @@ import {
   Asset, Dandiset, Paginated, User, Version, Info, AssetFile, AssetFolder,
 } from '@/types';
 
+if (!process.env.VUE_APP_DANDI_API_ROOT) {
+  throw new Error('Environment variable "VUE_APP_DANDI_API_ROOT" must be set.');
+}
+
 // Ensure contains trailing slash
 const dandiApiRoot = process.env.VUE_APP_DANDI_API_ROOT.endsWith('/')
   ? process.env.VUE_APP_DANDI_API_ROOT
@@ -12,12 +16,14 @@ const dandiApiRoot = process.env.VUE_APP_DANDI_API_ROOT.endsWith('/')
 
 const client = axios.create({ baseURL: dandiApiRoot });
 
-let oauthClient: OAuthClient|null;
+let oauthClient: OAuthClient|null = null;
 try {
-  oauthClient = new OAuthClient(
-    process.env.VUE_APP_OAUTH_API_ROOT,
-    process.env.VUE_APP_OAUTH_CLIENT_ID,
-  );
+  if (process.env.VUE_APP_OAUTH_API_ROOT && process.env.VUE_APP_OAUTH_CLIENT_ID) {
+    oauthClient = new OAuthClient(
+      process.env.VUE_APP_OAUTH_API_ROOT,
+      process.env.VUE_APP_OAUTH_CLIENT_ID,
+    );
+  }
 } catch (e) {
   oauthClient = null;
 }
